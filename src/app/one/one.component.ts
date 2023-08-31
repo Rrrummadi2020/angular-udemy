b32b51f5a6544c602fe111e79c913a6d10d80cce
Ummadi Rama Rangeswara Reddy <ramarangeswara.reddy@sen-sei.in>
2023-08-31 19:47:30 +0530

    emulated is the default

--- src/app/one/one.component.ts
+++ src/app/one/one.component.ts
@@ -3,7 +3,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-one',
   templateUrl: './one.component.html',
-  styleUrls: ['./one.component.css']
+  styleUrls: ['./one.component.css'],
+  encapsulation:ViewEncapsulation.Emulated
 })
 export class OneComponent {
 
--- src/app/two/two.component.ts
+++ src/app/two/two.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-two',
   templateUrl: './two.component.html',
-  styleUrls: ['./two.component.css']
+  styleUrls: ['./two.component.css'],
+  encapsulation:ViewEncapsulation.Emulated//default value for the encapsulation
 })
 export class TwoComponent {
 
