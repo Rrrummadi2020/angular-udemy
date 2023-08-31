b610af55bedf1d309776b4f0a61c5857209f53c9
Ummadi Rama Rangeswara Reddy <ramarangeswara.reddy@sen-sei.in>
2023-08-31 21:22:48 +0530

    observalble we can canel but promises not

--- src/app/two/two.component.ts
+++ src/app/two/two.component.ts
@@ -8,28 +8,38 @@ import { Observable, Subscription } from 'rxjs';
   encapsulation: ViewEncapsulation.None//default value for the encapsulation
 })
 export class TwoComponent {
-  mySubscription: Observable<string> | undefined;
+  myObservable: Observable<string> | undefined;
   myPromise: Promise<String> | undefined;
+  subsc: any;
+  counter: number | undefined;
   constructor() {
-
+    this.counter = 0;
   }
   create() {
     //observable lines will not execute oneline also
-    this.myPromise = new Promise((res, rej) => {
+    this.myPromise = new Promise((resolve, rej) => {
       console.log('promise before resolving ');
-      res('data resolved ');
+      resolve('data resolved ');
     });
     //promises line wiill execute the each line except the resolve data
-    this.mySubscription = new Observable<string>(observer => {
+    this.myObservable = new Observable<string>(observer => {
       console.log('before emmtting the observer');
-      observer.next('observer data created from ');
+      setInterval(() => {
+        observer.next('observer data created from ' + this.counter);
+        this.counter = this.counter || 0 + 1;
+      }, 1000);
     })
     // promised are eagerly executing while observalbles area lazily loaded
   }
   execute() {
     this.myPromise?.then(data => console.log(data));
-    this.mySubscription?.subscribe(data => {
+    this.subsc = this.myObservable?.subscribe(data => {
       console.log(data);
     })
+  }
+  cancel() {
+    if (this.subsc) {
+      this.subsc.unsubscribe()
+    }
   }
 }
