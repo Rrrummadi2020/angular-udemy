48a0d894342cfc81f36e5330eb83b8a51a2bd588
Ummadi Rama Rangeswara Reddy <ramarangeswara.reddy@sen-sei.in>
2023-09-05 12:27:51 +0530

    observable subscribing

--- src/app/one/one.component.ts
+++ src/app/one/one.component.ts
@@ -1,11 +1,35 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-one',
   templateUrl: './one.component.html',
   styleUrls: ['./one.component.css'],
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
-export class OneComponent {
+export class OneComponent implements OnInit {
+  counter!: number;
+  myObservable!: Observable<number[]>;
+  datas: number[] = []
 
+  ngOnInit(): void {
+    this.counter = 0;
+    this.myObservable = new Observable(observer => {
+      setInterval(() => {
+        this.counter++;
+        this.datas.push(this.counter);
+        observer.next(this.datas);
+        if(this.datas.length>5){
+          observer.complete();
+        }
+      }, 1000)
+    })
+    this.myObservable.subscribe(res=>{
+      console.log(res);
+    })
+  }
+
+  getData() {
+    console.log(this.datas);
+  }
 }
