ef34f33642e4e5109a639e2901e77af7694b106d
Rama U <ramarangeswarareddy123@gmail.com>
2023-11-27 22:37:20 +0530

    wildcard state change

--- src/app/animation-p/animation-p.component.ts
+++ src/app/animation-p/animation-p.component.ts
@@ -21,13 +21,44 @@ import { Component } from '@angular/core';
         'highlighted',
         style({ 'background-color': 'yellow', transform: 'translateX(100px)' })
       ),
-      transition('normal=>highlighted', animate(300)),
-      transition('highlighted=>normal', animate(600)),
+      // transition('normal=>highlighted', animate(300)),
+      // transition('highlighted=>normal', animate(600)),
+      transition('highlighted<=>normal', animate(600)),
+    ]),
+
+    trigger('wildState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0px) scale(1)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          'background-color': 'yellow',
+          transform: 'translateX(100px) scale(1)',
+        })
+      ),
+      state(
+        'shrunken',
+        style({
+          'background-color': 'orange',
+          transform: 'translateX(0px) scale(0.5)',
+        })
+      ),
+      // transition('normal=>highlighted', animate(300)),
+      // transition('highlighted=>normal', animate(600)),
+      transition('highlighted<=>normal', animate(600)),
+      transition('shrunken <=> *', animate(600)),
     ]),
   ],
 })
 export class AnimationPComponent {
   state!: string;
+  wild!: string;
+
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item: string) {
@@ -35,6 +66,10 @@ export class AnimationPComponent {
   }
   onAnimate() {
     this.state = this.state == 'normal' ? 'highlighted' : 'normal';
+    this.wild = this.wild == 'normal' ? 'highlighted' : 'normal';
+  }
+  onShrink() {
+    this.wild = 'shrunken';
   }
   onDelete(item: string) {}
 }
