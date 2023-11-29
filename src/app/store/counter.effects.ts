81cd9e1c56d3682b64bf426d0f78f301ae04bffe
Rama U <ramarangeswarareddy123@gmail.com>
2023-11-29 22:37:09 +0530

    latest value of counter saving into external

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,20 +1,28 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { decrement, increment } from './counter.action';
-import { tap } from 'rxjs';
+import { tap, withLatestFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 @Injectable()
 export class CounterEffects {
   saveCount = createEffect(
     () =>
       this.actions$.pipe(
         ofType(increment, decrement),
-        tap((action) => {
+        withLatestFrom(this.store.select('counter')),
+        tap(([action, counter]) => {
           console.log(action);
+          console.log(counter);
           console.log(action.value);
+          //operations of store like reducer,selector & action should be simple and synchronous,
+          //for sending action data to Http, log . or some other ,need to use Effects package
         })
       ),
     { dispatch: false }
   );
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private store: Store<{ counter: number }>
+  ) {}
 }
